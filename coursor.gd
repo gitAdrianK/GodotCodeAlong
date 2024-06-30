3344e51c7e3e8bfc9cd3dde6237163a2716af14a
Adrian K <14051526+gitAdrianK@users.noreply.github.com>
2024-06-30 18:55:32 +0200

    I'm canning this project

--- coursor.gd
+++ coursor.gd
@@ -1,20 +1,34 @@
 class_name Coursor
 extends Control
 
-var _offset
+@onready var held_domino = $HeldDomino
 
 
-func _process(_delta):
-	if get_child_count() == 0:
+func _physics_process(_delta):
+	if held_domino.get_child_count() == 0:
 		return
-	var coursor_pos = get_viewport().get_mouse_position()
-	get_child(0).set_position(coursor_pos + _offset)
+	var coursor_pos = get_global_mouse_position()
+	var domino : Domino = held_domino.get_child(0)
+	var half_x = domino.size.x / 2
+	var half_y = domino.size.y / 2
+	domino.position = coursor_pos - Vector2(half_x, half_y)
+
+func _input(event):
+	if held_domino.get_child_count() == 0:
+		return
+	if not event is InputEventMouseButton:
+		return
+	if not event.is_pressed():
+		return
+	if event.button_index == MOUSE_BUTTON_MASK_RIGHT:
+		var domino : Domino = held_domino.get_child(0)
+		domino.rotation_degrees = domino.rotation_degrees + 90
 
 
 func add_domino_to_coursor(domino : Domino):
-	domino.reparent(self)
+	domino.reparent(held_domino)
 	domino.connect("domino_released", _remove_domino_from_coursor)
-	_offset = domino.get_screen_position() - get_viewport().get_mouse_position()
+	domino.scale = Vector2(1.2, 1.2)
 
 
 func _remove_domino_from_coursor(domino : Domino):
@@ -22,4 +36,5 @@ func _remove_domino_from_coursor(domino : Domino):
 	var game_node : Game = get_tree().get_root().get_child(0)
 	var playingfield : Playingfield = game_node.playingfield
 	playingfield.add_domino_to_playingfield(domino)
-
+	domino.disconnect("domino_released", _remove_domino_from_coursor)
+	domino.scale = Vector2(1, 1)
